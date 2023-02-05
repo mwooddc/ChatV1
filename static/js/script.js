a89c7130a816ce6ebd4f3f3bf5cc79117570da23
mrmwood <markwood1981@hotmail.com>
2023-02-05 22:12:11 +0400

    confirm delete resolved with css

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 function get_userID(userID)
 {
     console.log(userID)
+
     console.log(userID.firstElementChild)
     // document.getElementById('the_userID').textContent = x.textContent;
     userID = userID.firstElementChild.textContent
@@ -8,3 +9,14 @@ function get_userID(userID)
     document.getElementById('the_userID_field').value = userID
     return userID
 }
+
+function getvalue(){
+    var user = document.getElementById('the_userID_field').value;
+    console.log(user)
+    if (user == ""){
+      console.log("x is Empty")
+      document.getElementById('userID_in_modal').innerText = "No user selected";
+    }else{
+    document.getElementById('userID_in_modal').innerText = `Are you sure that you want to delete user: ${user}`
+    }
+  }
